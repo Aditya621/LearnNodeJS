dc6d4285474ce12eaebcb8ffb53468207fdf5dc3
Aditya Singh <thakuraditya621@gmail.com>
2022-11-22 22:44:48 +0530

    add pages

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const http = require("http");
 const path = require("node:path/win32");
 const url = require("url");
+
+const replaceTemplate = require("./1-node-farm/starter/modules/replaceTemplate");
+
 //////////////////////////////////////////
 // FILES
 //this is Synchronous Way of readd a file
@@ -30,23 +33,6 @@ const url = require("url");
 //i want my data will be called once
 // if i am using readFile in callBack then it will called every time when user send a request to SERVER
 // so i am using it a top level
-
-const replaceTemplate = (temp_cards, product) => {
-  let output = temp_cards.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/1-node-farm/starter/templates/template-overview.html`,
