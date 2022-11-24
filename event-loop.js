6b76c67cb7be7d95a620095a270d4fd93a47ad13
Aditya Singh <thakuraditya621@gmail.com>
2022-11-24 21:09:07 +0530

    Node_Behind_Scene

--- event-loop.js
+++ event-loop.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const crypto = require('crypto');
+const start = Date.now();
+
+//we can also change the ThreadPool
+process.env.UV_THREADPOOL_SIZE = 1;
 
 setTimeout(() => {
   console.log('timer  1 finished');
@@ -23,5 +28,22 @@ fs.readFile(`${__dirname}/how-node-js-work/starter/test-file`, () => {
 
   process.nextTick(() => {
     console.log('process.nextTick');
+  });
+
+  // some work with ThreadPool which is 4 byDefault in NOde JS
+  //Event Loop acure only when clallback is in Asynchronous
+  //like
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password Encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password Encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password Encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password Encrypted');
   });
 });
