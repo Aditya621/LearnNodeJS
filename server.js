2003e14b712c133eb7fe12321fcc3667ae5a9c84
Aditya Singh <thakuraditya621@gmail.com>
2022-11-27 20:06:56 +0530

    connect DataBase

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
 
 dotenv.config({ path: './.env' });
 
-// console.log(process.env);
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+// console.log(DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection established Successfully');
+  });
 // Start Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
